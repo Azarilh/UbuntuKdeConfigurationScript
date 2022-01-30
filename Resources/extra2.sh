bbefbb9c7f83c070df74961d365cf5d7c06f991a
Azarilh Namu <40278785+Azarilh@users.noreply.github.com>
2022-01-30 14:15:23 +0100

    Update extra2.sh

--- Resources/extra2.sh
+++ Resources/extra2.sh
@@ -29,20 +29,20 @@ export LC_MESSAGES="en_IE.UTF-8"
 export GTK_USE_PORTAL=1' >> /etc/environment
 
 # Edit .bashrc
-echo "#alias in="sudo pacman -S --needed"             #install packages
-#alias rin="sudo pacman -S"                     #reinstall packages
-#alias up="sudo pacman -Sy"                     #update repositories
-#alias Syu="sudo pacman -Syu"                   #upgrade all
-#alias upg="sudo pacman -U"                     #upgrade packages
-#alias re="sudo pacman -R"                      #remove packages
+echo '#alias in="sudo pacman -S --needed"             #install packages
+#alias rin="sudo pacman -S"                           #reinstall packages
+#alias up="sudo pacman -Sy"                           #update repositories
+#alias Syu="sudo pacman -Syu"                         #upgrade all
+#alias upg="sudo pacman -U"                           #upgrade packages
+#alias re="sudo pacman -R"                            #remove packages
 
-alias in="sudo apt install"                     #install packages
-alias rin="sudo apt install --reinstall"        #reinstall packages
-alias up="sudo apt update && sudo apt list --upgradable"                        #>
-alias upg="sudo apt upgrade"                    #upgrade packages/upgrade all
-alias re="sudo apt remove"                      #remove packages
-alias pu="sudo apt purge"                       #purge packages
-alias ar="sudo apt autoremove"                  #remove outdated packages
+alias in="sudo apt install"                           #install packages
+alias rin="sudo apt install --reinstall"              #reinstall packages
+alias up="sudo apt update && apt list --upgradable"   #update repos
+alias upg="sudo apt upgrade"                          #upgrade packages/upgrade all
+alias re="sudo apt remove"                            #remove packages
+alias pu="sudo apt purge"                             #purge packages
+alias ar="sudo apt autoremove"                        #remove outdated packages
 alias upgrade-kubuntu="sudo do-release-upgrade -m desktop -f DistUpgradeViewKDE"
 alias drivetestwrite="sudo /sbin/sysctl -w vm.drop_caches=3 && sync; dd if=/dev/z>
 alias drivetestread="sudo /sbin/sysctl -w vm.drop_caches=3 && dd if=tempfile of=/>
@@ -53,7 +53,7 @@ export GTK_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
 export QT_IM_MODULE=fcitx
 export DISPLAY=:0.0
-QT4_IM_MODULE=fcitx" > /home/azarilh/.bashrc
+QT4_IM_MODULE=fcitx' > /home/azarilh/.bashrc
 
 journalctl --vacuum-size=200M
 
